11a3e98f3dd4f6a42a2be4df629fed1d5dddd3e6
sally <sally@atlasagency.com.au>
2015-12-14 13:34:28 +1100

    angular app to heroku

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name internalProjectBmwApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the internalProjectBmwApp
+ */
+angular.module('internalProjectBmwApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name internalProjectBmwApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the internalProjectBmwApp
+ */
+angular.module('internalProjectBmwApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
